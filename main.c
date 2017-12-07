e7b5b0e776fadbe081608326b5d32ca71c21878b
Fabien Lheureux <fabien.lheureux@trust-in-soft.com>
2017-12-07 20:00:17 +0100

    Check all possible shift

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <caesar.h>
+#include <tis_builtin.h>
 
 void gen_test(int user_shift, int len, char *str)
 {
@@ -9,12 +10,10 @@ void gen_test(int user_shift, int len, char *str)
     printf("Encrypt text '%s'\n", str);
     res1 = encrypt(str, len, user_shift);
     if (!res1) return;
-    printf("Result: %s\n", res1);
 
-    printf("Decrypt text '%s'\n", res1);
-    res2 = decrypt(res1, len, user_shift);
+    printf("Decrypt text '%s'\n", str);
+    res2 = decrypt(str, len, user_shift);
     if (!res2) return;
-    printf("Result: %s\n", res2);
 
     free(res1);
     free(res2);
@@ -31,6 +30,11 @@ int main(void)
     gen_test(-3, len, str);
     printf("\nTest 2: Shift with a positive input\n");
     gen_test(7, len, str);
+
+    printf("\nTest 3: Shift with all possible input\n");
+    int user_shift;
+    tis_make_unknown(&user_shift, sizeof user_shift);
+    gen_test(user_shift, len, str);
 
     return 0;
 }
